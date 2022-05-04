2793f66cc00524fe0bb4d1047fc1451d1862ded1
Vannarith Om Jr <vannarithomjr@swuser-129-8-214-152.swlan.csufresno.edu>
2022-05-04 11:17:25 -0700

    english portion done

---
+++ lab_6.py
@@ -0,0 +1,12 @@
+"""
+To start, we will generate a random integer between 1 and 20.
+Based on the result of the random number, we check to see if it falls under certain range.
+If the number is grater than 15, then the result will be "Cherries".
+Otherwise if the number is > 10, then the result will be "Orange".
+Otherwise if the number is > 5, then the result will be "Plum".
+Otherwise if the number is > 2, then the result will be "Melon".
+Otherwise if the number is > 1, then the result will be "Bell".
+If the number is not any of the above, then the result will be "Bar".
+We iterate over using a loop thtee times and print the results to the user.
+As an example, "Plum Cherries Melon"
+"""
