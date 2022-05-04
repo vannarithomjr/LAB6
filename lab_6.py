7658a895ab50a4142343775aa944383ea23e6898
Vannarith Om Jr <vannarithomjr@swuser-129-8-214-152.swlan.csufresno.edu>
2022-05-04 12:27:28 -0700

    code done

--- lab_6.py
+++ lab_6.py
@@ -30,3 +30,66 @@ otherwise if the number is > 1
 loop three times
     print the output (fruit) to the user
 """
+
+import random
+
+total_earned = 0    # global
+
+def main(total_earned):
+    results = []
+    for i in range(0, 3):
+        results.append(spin())
+
+    print("results:", results)
+    winner = jackpot(results)
+
+    if (winner):
+        print("Winner winner chicken dinner!")
+    else:
+        print("You are out of luck. Try again, if you dare.")
+
+    grand_total = count(results, total_earned)
+    print("grand_total", grand_total)
+    option = input("Would you like to play again? [y] or [n]: ")
+    if option.lower() == "y":
+        main(total_earned)
+
+def spin():
+    rand_num = random.randint(1, 20)
+    output  = ""
+    if rand_num > 15:
+        output = "Cherries"
+    elif rand_num > 10:
+        output = "Orange"
+    elif rand_num > 5:
+        output = "Plum"
+    elif rand_num > 2:
+        output = "Bell"
+    elif rand_num > 1:
+        output = "Melon"
+    else:
+        output = "Bar"
+
+    print(output, end = " ")
+    return output
+
+
+def jackpot(results):
+    return results[0] == results[1] == results[2]
+
+def count(results, total_earned):
+    total = 0
+    money_dict = {
+        "Cherries" : 1,
+        "Orange" : .7,
+        "Plum" : .6,
+        "Bell" : .4,
+        "Melon" : .2,
+        "Bar" : .1
+    }
+
+    for i in results:
+        total += money_dict[i]
+    return total_earned + total
+
+main(total_earned)
